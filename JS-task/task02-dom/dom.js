b2784670f5713d4ce99d4469c694f60b2531bf9e
KnifeSGS <knife.sgs@gmail.com>
2021-04-30 10:54:39 +0200

    2 feladat kész

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,22 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+  const targetLinks = document.querySelectorAll('nav#link-list a');
+  targetLinks.forEach(link => {
+    if (link.innerHTML.includes('outer-link')) {
+      link.setAttribute('target', '_blank');
+      link.innerHTML = `<strong>${link.innerHTML}</strong>`;
+    }
+  })
 
+  document.querySelectorAll("nav").forEach((e) => {
+    e.style.display = "flex";
+    e.style.flexDirection = "column";
+    e.style.margin = "0px auto";
+    e.style.width = "30%";
+    e.style.border = "1px solid blue";
+    e.style.padding = "16px";
+  });
+}
+
+changeOuterLinks()
 
 export { changeOuterLinks };
